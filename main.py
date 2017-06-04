e00dad9f5235cd8ad2bd26c89ce2f1e636f9f896
Paul Klinger <klinger.paul@gmail.com>
2017-06-05 01:51:29 +0200

    added gui

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import csv
 import datetime
 import dateutil.parser
 import argparse
+
+import multiprocessing
+import queue
+import io
+import sys
+
 from typing import List, NewType, Union, Tuple
 
 AbsoluteUrl = NewType("AbsoluteUrl", str)
@@ -18,10 +24,15 @@ SIGNIN_URL = AbsoluteUrl("https://www.goodreads.com/user/sign_in")
 
 
 parser = argparse.ArgumentParser(description=
-                                 """Adds genre and (re)reading dates information to a goodreads export file.""")
-parser.add_argument("--csv", required=True)
-parser.add_argument("--email", required=True)
-parser.add_argument("--password", required=True)
+                                 """Adds genre and (re)reading dates information to a GoodReads export file.""")
+parser.add_argument("-c", "--csv", help="path of your GoodReads export file")
+parser.add_argument("-e", "--email", help="the email you use to login to GoodReads")
+parser.add_argument("-p", "--password", help="your GoodReads Password")
+
+parser.add_argument("-f", "--force", action="store_true",
+                    help="process all books (by default only those without genre information are processed)")
+
+parser.add_argument("-g", "--gui", action="store_true", help="show GUI")
 
 options = parser.parse_args()
 
@@ -34,35 +45,52 @@ STANDARD_FIELDNAMES = ["Book Id", "Title", "Author", "Author l-f", "Additional A
 
 
 def sign_in(email: str, password: str) -> requests.Session:
-    session = requests.Session()
-    print("Getting login page")
-    response = session.get(SIGNIN_URL)
-    soup = BeautifulSoup(response.content, "html.parser")
-    auth_token = soup.find(attrs={"name": "authenticity_token"})["value"]
-    n_token = soup.find(attrs={"name": "n"})["value"]
+    try:
+        session = requests.Session()
+        print("Getting login page")
+        response = session.get(SIGNIN_URL)
+        soup = BeautifulSoup(response.content, "html.parser")
+        auth_token = soup.find(attrs={"name": "authenticity_token"})["value"]
+        n_token = soup.find(attrs={"name": "n"})["value"]
 
-    form_data = {"authenticity_token": auth_token, "user[email]": email, "user[password]": password,
-                 "next": "Sign in", "n": n_token, "remember_me": "on", "utf8": "✓"}
-    print("Logging in")
-    login_response = session.post(SIGNIN_URL, data=form_data)
-    login_response.raise_for_status()
+        form_data = {"authenticity_token": auth_token, "user[email]": email, "user[password]": password,
+                     "next": "Sign in", "n": n_token, "remember_me": "on", "utf8": "✓"}
+        print("Logging in")
+        login_response = session.post(SIGNIN_URL, data=form_data)
+        login_response.raise_for_status()
+
+    except requests.RequestException as e:
+        print(f"Error logging in: {e}")
+        sys.exit()
+
+    except KeyError:
+        print(f"error parsing login page, maybe layout changed?")
+        sys.exit()
+
     return session
 
 
 def parse_csv(filename: Path):
-    with open(filename, newline="", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-        if set(reader.fieldnames) < set(STANDARD_FIELDNAMES):
-            raise ValueError("CSV file does not contain the standard fieldnames!")
-        return list(reader)
+    try:
+        with open(filename, newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            if set(reader.fieldnames) < set(STANDARD_FIELDNAMES):
+                raise ValueError("CSV file does not contain the standard fieldnames!")
+            return list(reader)
+    except (ValueError, csv.Error, IOError) as e:
+        print(f"Error reading export file: {e}")
+        sys.exit()
 
 
 def write_csv(data: List[dict], fieldnames: List[str], filename: Path):
-    with open(filename, "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames,
-                                delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writeheader()
-        writer.writerows(data)
+    try:
+        with open(filename, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames,
+                                    delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writeheader()
+            writer.writerows(data)
+    except (IOError, csv.Error):
+        print(f"Error writing export file: {e}")
 
 
 @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_tries=10)
@@ -81,6 +109,9 @@ def get_read_dates(soup: BeautifulSoup) -> List[Tuple[datetime.datetime, datetim
     if allReadingSessions is None:
         return []
     timeline = allReadingSessions.find(class_="readingTimeline")
+    if timeline is None:
+        print("Error finding read dates, skipping.")
+        return []
 
     status_updates = []
     for entry in timeline.findAll(class_="readingTimeline__text"):
@@ -126,12 +157,10 @@ def get_genres(soup):
     return genres
 
 
-def main():
-    options = parser.parse_args()
-    books = parse_csv(options.csv)
-    session = sign_in(options.email, options.password)
-
-    books_to_process = [b for b in books if not b.get("genres", None)]
+def enhance_export(options: dict):
+    books = parse_csv(options["csv"])
+    session = sign_in(options["email"], options["password"])
+    books_to_process = [b for b in books if (options["force"] or not b.get("genres", None))]
     for i, book in enumerate(books_to_process):
         print(f"Book {i+1} of {len(books_to_process)}: {book['Title']} ({book['Author']})")
         page = get_with_retry(session, make_book_url(book["Book Id"]))
@@ -142,11 +171,148 @@ def main():
         book["read_dates"] = ";".join(",".join(d.strftime("%Y-%m-%d") if d else "" for d in reading)
                                       for reading in read_dates)
         book["genres"] = ";".join(f"{','.join(genre[0])}|{genre[1]}" for genre in genres)
-        #print(f"read dates: {read_dates}")
-        #print(f"genres: {genres}")
 
         if i % 20 == 0 or i == len(books_to_process) - 1:
             print("saving csv")
             write_csv(books, STANDARD_FIELDNAMES + ["read_dates", "genres"], "export_enhanced.csv")
 
-main()
+
+class IOQueue(object):  # only used for gui, needs to be defined outside of launch_gui for multiprocessing
+    def __init__(self, queue: queue.Queue):
+        self.queue = queue
+
+    def write(self, text):
+        self.queue.put(text)
+
+    def flush(self):
+        pass
+
+
+def task(options: dict, stdout_queue: queue.Queue):
+    sys.stdout = IOQueue(stdout_queue)
+    enhance_export(options)
+
+
+def launch_gui():
+    import tkinter as tk
+    from tkinter import ttk
+    from tkinter.filedialog import askopenfilename
+
+    class IOText(ttk.Frame):
+        def __init__(self, text_queue: multiprocessing.Queue, *args, **kwargs):
+            ttk.Frame.__init__(self, *args, **kwargs)
+
+            self.text = tk.Text(self, height=6, width=100)
+            self.vsb = ttk.Scrollbar(self, orient="vertical", command=self.text.yview)
+            self.text.configure(yscrollcommand=self.vsb.set)
+            self.vsb.pack(side="right", fill="y")
+            self.text.pack(side="left", fill="both", expand=True)
+            self.queue = text_queue
+            self.update()
+
+        def update(self):
+            try:
+                text = self.queue.get_nowait()
+            except queue.Empty:
+                text = None
+
+            if text:
+                scroll = False
+                if self.text.dlineinfo('end-1chars') is not None:  # autoscroll if at end
+                    scroll = True
+                self.text.insert("end", text)
+                if scroll:
+                    self.text.see("end")
+
+            self.after(100, self.update)
+
+    def ask_for_filename():
+        filename = askopenfilename()
+        pathlabel.config(text=filename)
+
+    def start_processing():
+        options = {
+            "csv": pathlabel["text"],
+            "force": forceentry.instate(["selected"]),
+            "email": emailentry.get(),
+            "password": passwordentry.get()
+        }
+
+        process = multiprocessing.Process(target=task, args=(options, stdout_queue), daemon=True)
+        process.start()
+
+        def check_if_finished():
+            if process.is_alive():
+                root.after(300, check_if_finished)
+            else:
+                change_all_state(tk.NORMAL)
+
+        change_all_state(tk.DISABLED)
+        check_if_finished()
+
+    def change_all_state(new_state):
+        filebutton["state"] = new_state
+        emailentry["state"] = new_state
+        passwordentry["state"] = new_state
+        forceentry["state"] = new_state
+        if new_state == tk.NORMAL:
+            forceentry.state(["!alternate", "!selected"])
+        start_button["state"] = new_state
+
+    root = tk.Tk()
+    root.wm_title("Enhance GoodReads Export Tool")
+    frame = ttk.Frame(root, padding="3m")
+    frame.pack(fill=tk.BOTH)
+    filebutton = ttk.Button(frame, text="export file", command=ask_for_filename)
+    filebutton.grid(row=0, column=0)
+    pathlabel = ttk.Label(frame, anchor=tk.W)
+    pathlabel.grid(row=0, column=1, columnspan=10, sticky=tk.W)
+
+    emaillabel = ttk.Label(frame, text="email:")
+    emailentry = ttk.Entry(frame)
+    emaillabel.grid(row=1, column=0)
+    emailentry.grid(row=1, column=1)
+    passwordlabel = ttk.Label(frame, text="password")
+    passwordentry = ttk.Entry(frame)
+    passwordlabel.grid(row=2, column=0)
+    passwordentry.grid(row=2, column=1)
+    forcelabel = ttk.Label(frame, text="process all")
+    forceentry = ttk.Checkbutton(frame)
+    forceentry.state(["!alternate", "!selected"])
+    forcehelp = ttk.Label(frame, text="(by default only books without genre information are processed)")
+    forcelabel.grid(row=3, column=0)
+    forceentry.grid(row=3, column=1)
+    forcehelp.grid(row=3, column=2)
+
+    start_button = ttk.Button(frame, text="start processing", command=start_processing)
+    start_button.grid(row=4, column=0, columnspan=2, pady=5)
+
+    frame.grid_columnconfigure(0, weight=0)
+    frame.grid_columnconfigure(1, weight=0)
+    frame.grid_columnconfigure(2, weight=0)
+    frame.grid_columnconfigure(3, weight=1)
+
+    stdout_queue = multiprocessing.Queue()
+    stdout_queue.cancel_join_thread()
+    info = IOText(stdout_queue, frame)
+    info.grid(row=10, column=0, columnspan=10, sticky=tk.N+tk.S+tk.E+tk.W)
+
+    root.mainloop()
+
+
+def main():
+    options = vars(parser.parse_args())
+    if options["gui"]:
+        launch_gui()
+        return
+
+    if not all((options["email"], options["password"], options["csv"])):
+        print("You need to provide the path to the export file, an email address and a password!")
+        parser.print_help()
+        return
+
+    enhance_export(options)
+
+
+if __name__ == "__main__":
+    main()
