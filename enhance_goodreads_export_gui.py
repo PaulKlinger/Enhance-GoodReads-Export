16733857748889a5a742ae451d217afa81229c8a
Paul Klinger <klinger.paul@gmail.com>
2019-01-02 00:43:38 +0100

    new version of pyinstaller doesn't need the workaround
    (it causes a max recursion error if it is added anyway)

--- enhance_goodreads_export_gui.py
+++ enhance_goodreads_export_gui.py
@@ -1,48 +1,12 @@
 import multiprocessing
-import multiprocessing.popen_spawn_win32 as forking
 import queue
 import sys
-import os
 
 import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 
 from enhance_goodreads_export import enhance_export, EnhanceExportException
-
-
-#########################################################################################
-# Workaround for multiprocessing when using pyinstaller to package into one executable
-# see https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
-#########################################################################################
-
-class _Popen(forking.Popen):
-    def __init__(self, *args, **kw):
-        if hasattr(sys, 'frozen'):
-            # We have to set original _MEIPASS2 value from sys._MEIPASS
-            # to get --onefile mode working.
-            os.putenv('_MEIPASS2', sys._MEIPASS)
-        try:
-            super(_Popen, self).__init__(*args, **kw)
-        finally:
-            if hasattr(sys, 'frozen'):
-                # On some platforms (e.g. AIX) 'os.unsetenv()' is not
-                # available. In those cases we cannot delete the variable
-                # but only set it to the empty string. The bootloader
-                # can handle this case.
-                if hasattr(os, 'unsetenv'):
-                    os.unsetenv('_MEIPASS2')
-                else:
-                    os.putenv('_MEIPASS2', '')
-
-
-# Second override 'Popen' class with our modified version.
-forking.Popen = _Popen
-
-
-#########################################################################################
-# end of workaround
-#########################################################################################
 
 
 class IOQueue(object):
