e05e786ab24e44bd09292a880724dc86584de2bc
Paul Klinger <klinger.paul@gmail.com>
2022-05-12 21:20:38 +0100

    refactor gui

--- enhance_goodreads_export/enhance_goodreads_export_gui.py
+++ enhance_goodreads_export/enhance_goodreads_export_gui.py
@@ -20,6 +20,12 @@ class IOQueue(object):
         pass
 
 
+def human_tk_captcha_solver(captcha_data: bytes) -> str:
+    with open("captcha.jpg", "wb") as f:
+        f.write(captcha_data)
+    return input("Please enter the characters in the captcha:").strip().lower()
+
+
 def task(options: dict, stdout_queue: queue.Queue):
     sys.stdout = IOQueue(stdout_queue)  # type: ignore
     try:
@@ -57,107 +63,118 @@ class IOText(ttk.Frame):
         self.after(100, self.update)
 
 
-def launch_gui():
-    def ask_for_filename():
-        filename = askopenfilename()
-        pathlabel.config(text=filename)
+class EnhanceExportGui(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.wm_title("Enhance GoodReads Export Tool")
+        self.resizable(False, False)
+        self.frame = ttk.Frame(self, padding="3m")
+        self.frame.pack(fill=tk.BOTH)
+        self.filebutton = ttk.Button(
+            self.frame, text="export file", command=self.ask_for_filename
+        )
+        self.filebutton.grid(row=0, column=0, sticky=tk.W)
+        self.pathlabel = ttk.Label(
+            self.frame,
+            anchor=tk.W,
+            text="[Goodreads export file, new columns will be added to this file]",
+        )
+        self.pathlabel.grid(row=0, column=1, columnspan=10, sticky=tk.W)
 
-    def ask_for_update_filename():
-        filename = askopenfilename()
-        update_pathlabel.config(text=filename)
+        self.update_filebutton = ttk.Button(
+            self.frame, text="old file", command=self.ask_for_update_filename
+        )
+        self.update_filebutton.grid(row=1, column=0)
+        self.update_pathlabel = ttk.Label(
+            self.frame,
+            anchor=tk.W,
+            text="[previously enhanced file to copy values from (optional)]",
+        )
+        self.update_pathlabel.grid(row=1, column=1, columnspan=10, sticky=tk.W)
 
-    def start_processing():
+        self.emaillabel = ttk.Label(self.frame, text="email:")
+        self.emailentry = ttk.Entry(self.frame)
+        self.emaillabel.grid(row=2, column=0)
+        self.emailentry.grid(row=2, column=1)
+        self.passwordlabel = ttk.Label(self.frame, text="password")
+        self.passwordentry = ttk.Entry(self.frame)
+        self.passwordlabel.grid(row=3, column=0)
+        self.passwordentry.grid(row=3, column=1)
+        self.forcelabel = ttk.Label(self.frame, text="process all")
+        self.forceentry = ttk.Checkbutton(self.frame)
+        self.forceentry.state(["!alternate", "!selected"])
+        forcehelp = ttk.Label(
+            self.frame,
+            text="(by default only books without genre information are processed)",
+        )
+        self.forcelabel.grid(row=4, column=0)
+        self.forceentry.grid(row=4, column=1)
+        forcehelp.grid(row=4, column=2)
+
+        self.start_button = ttk.Button(
+            self.frame, text="start processing", command=self.start_processing
+        )
+        self.start_button.grid(row=5, column=0, columnspan=2, pady=5)
+
+        self.frame.grid_columnconfigure(0, weight=0)
+        self.frame.grid_columnconfigure(1, weight=0)
+        self.frame.grid_columnconfigure(2, weight=0)
+        self.frame.grid_columnconfigure(3, weight=1)
+
+        self.stdout_queue = multiprocessing.Queue()
+        self.stdout_queue.cancel_join_thread()
+        info = IOText(self.stdout_queue, self.frame)
+        info.grid(row=10, column=0, columnspan=10, sticky=tk.N + tk.S + tk.E + tk.W)
+
+    def ask_for_filename(self) -> None:
+        filename = askopenfilename()
+        self.pathlabel.config(text=filename)
+
+    def ask_for_update_filename(self) -> None:
+        filename = askopenfilename()
+        self.update_pathlabel.config(text=filename)
+
+    def start_processing(self) -> None:
         options = {
-            "csv": pathlabel["text"] if not pathlabel["text"].startswith("[") else "",
-            "update": update_pathlabel["text"]
-            if not update_pathlabel["text"].startswith("[")
+            "csv": self.pathlabel["text"]
+            if not self.pathlabel["text"].startswith("[")
             else "",
-            "force": forceentry.instate(["selected"]),
-            "email": emailentry.get(),
-            "password": passwordentry.get(),
+            "update": self.update_pathlabel["text"]
+            if not self.update_pathlabel["text"].startswith("[")
+            else "",
+            "force": self.forceentry.instate(["selected"]),
+            "email": self.emailentry.get(),
+            "password": self.passwordentry.get(),
         }
 
         process = multiprocessing.Process(
-            target=task, args=(options, stdout_queue), daemon=True
+            target=task, args=(options, self.stdout_queue), daemon=True
         )
         process.start()
 
         def check_if_finished():
             if process.is_alive():
-                root.after(300, check_if_finished)
+                self.after(300, check_if_finished)
             else:
-                change_all_state(tk.NORMAL)
+                self.change_all_state(tk.NORMAL)
 
-        change_all_state(tk.DISABLED)
+        self.change_all_state(tk.DISABLED)
         check_if_finished()
 
-    def change_all_state(new_state):
-        filebutton["state"] = new_state
-        update_filebutton["state"] = new_state
-        emailentry["state"] = new_state
-        passwordentry["state"] = new_state
-        forceentry["state"] = new_state
+    def change_all_state(self, new_state) -> None:
+        self.filebutton["state"] = new_state
+        self.update_filebutton["state"] = new_state
+        self.emailentry["state"] = new_state
+        self.passwordentry["state"] = new_state
+        self.forceentry["state"] = new_state
         if new_state == tk.NORMAL:
-            forceentry.state(["!alternate", "!selected"])
-        start_button["state"] = new_state
+            self.forceentry.state(["!alternate", "!selected"])
+        self.start_button["state"] = new_state
 
-    root = tk.Tk()
-    root.wm_title("Enhance GoodReads Export Tool")
-    root.resizable(False, False)
-    frame = ttk.Frame(root, padding="3m")
-    frame.pack(fill=tk.BOTH)
-    filebutton = ttk.Button(frame, text="export file", command=ask_for_filename)
-    filebutton.grid(row=0, column=0, sticky=tk.W)
-    pathlabel = ttk.Label(
-        frame,
-        anchor=tk.W,
-        text="[Goodreads export file, new columns will be added to this file]",
-    )
-    pathlabel.grid(row=0, column=1, columnspan=10, sticky=tk.W)
 
-    update_filebutton = ttk.Button(
-        frame, text="old file", command=ask_for_update_filename
-    )
-    update_filebutton.grid(row=1, column=0)
-    update_pathlabel = ttk.Label(
-        frame,
-        anchor=tk.W,
-        text="[previously enhanced file to copy values from (optional)]",
-    )
-    update_pathlabel.grid(row=1, column=1, columnspan=10, sticky=tk.W)
-
-    emaillabel = ttk.Label(frame, text="email:")
-    emailentry = ttk.Entry(frame)
-    emaillabel.grid(row=2, column=0)
-    emailentry.grid(row=2, column=1)
-    passwordlabel = ttk.Label(frame, text="password")
-    passwordentry = ttk.Entry(frame)
-    passwordlabel.grid(row=3, column=0)
-    passwordentry.grid(row=3, column=1)
-    forcelabel = ttk.Label(frame, text="process all")
-    forceentry = ttk.Checkbutton(frame)
-    forceentry.state(["!alternate", "!selected"])
-    forcehelp = ttk.Label(
-        frame, text="(by default only books without genre information are processed)"
-    )
-    forcelabel.grid(row=4, column=0)
-    forceentry.grid(row=4, column=1)
-    forcehelp.grid(row=4, column=2)
-
-    start_button = ttk.Button(frame, text="start processing", command=start_processing)
-    start_button.grid(row=5, column=0, columnspan=2, pady=5)
-
-    frame.grid_columnconfigure(0, weight=0)
-    frame.grid_columnconfigure(1, weight=0)
-    frame.grid_columnconfigure(2, weight=0)
-    frame.grid_columnconfigure(3, weight=1)
-
-    stdout_queue = multiprocessing.Queue()
-    stdout_queue.cancel_join_thread()
-    info = IOText(stdout_queue, frame)
-    info.grid(row=10, column=0, columnspan=10, sticky=tk.N + tk.S + tk.E + tk.W)
-
-    root.mainloop()
+def launch_gui() -> None:
+    gui = EnhanceExportGui()
+    gui.mainloop()
 
 
 if __name__ == "__main__":
