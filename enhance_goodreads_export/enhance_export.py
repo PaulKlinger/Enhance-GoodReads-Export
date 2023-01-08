0002d635dd866893f6e730d314ebe533a17d051c
Paul Klinger <klinger.paul@gmail.com>
2023-01-08 21:29:03 +0000

    handle escaped quotes on book page, minor fixes, reorg

--- enhance_goodreads_export/enhance_export.py
+++ enhance_goodreads_export/enhance_export.py
@@ -126,15 +126,52 @@ def get_genres(soup: BeautifulSoup) -> list[tuple[list[str], int]]:
                 (lines[0].strip(), int("".join(c for c in lines[1] if c.isdigit())))
             )
     genres.sort(key=lambda x: x[1], reverse=True)
+    # format genre name
+    genres = [(g[0].replace("-", " ").title(), g[1]) for g in genres]
+
+    # filter out useless shelves (e.g. to-read) and ones with less than 10 people
+    genres = [g for g in genres if g[1] >= 10 and valid_genre(g[0])]
+
     # genres used to support nested subgenres, this doesn't exist on the new book page.
     # To match the old format, treat all genres as 1 level (wrap name in list)
     genres = [([g[0]], g[1]) for g in genres]
 
-    # filter out useless shelves (e.g. to-read) and ones with less than 10 people
-    genres = [g for g in genres if g[1] >= 10 and valid_genre(g[0][0])]
-    # format genre name
-    genres = [(g[0].replace("-", " ").title(), g[1]) for g in genres]
     return genres[:20]
+
+
+@backoff.on_exception(backoff.expo, Exception, max_tries=3, max_time=2)
+def update_book_data(book: dict[str, str], session: requests.Session) -> None:
+    book_id = book["Book Id"]
+
+    review_page = get_with_retry(session, make_review_url(book_id))
+    review_soup = BeautifulSoup(review_page.content, "html.parser")
+    read_dates = get_read_dates(review_soup)
+    book["read_dates"] = ";".join(
+        ",".join(d.strftime("%Y-%m-%d") if d else "" for d in reading)
+        for reading in read_dates
+    )
+
+    book_page = get_with_retry(session, make_book_url(book_id)).content.decode("utf-8")
+    n_ratings_match = re.search(
+        r'(?:"|&quot;)ratingsCount(?:"|&quot;)\s*:\s*(\d+)', book_page
+    )
+    if n_ratings_match is None:
+        print(book_page)
+        raise ValueError("Did not find number of ratings in book page!")
+    book["n_ratings"] = n_ratings_match.group(1)
+
+    shelves_url_match = re.search(
+        '(?:"|&quot;)[^"&]*(work/shelves[^"&]+)(?:"|&quot;)', book_page
+    )
+    if shelves_url_match is None:
+        print("Did not find link to shelves page on book page, not adding genres!")
+        return
+    shelves_url = AbsoluteUrl(f"{BASE_URL}/{shelves_url_match.group(1)}")
+
+    genres_page = get_with_retry(session, shelves_url)
+    genres_soup = BeautifulSoup(genres_page.content, "html.parser")
+    genres = get_genres(genres_soup)
+    book["genres"] = ";".join(f"{','.join(genre[0])}|{genre[1]}" for genre in genres)
 
 
 def enhance_export(options: dict, captcha_solver: CaptchaSolver | None = None) -> None:
@@ -157,8 +194,9 @@ def enhance_export(options: dict, captcha_solver: CaptchaSolver | None = None) -
                 and ob["Exclusive Shelf"] == b["Exclusive Shelf"]
                 and ob["Date Read"] == b["Date Read"]
             ):
-                b["read_dates"] = old_books_by_id[b["Book Id"]]["read_dates"]
-                b["genres"] = old_books_by_id[b["Book Id"]]["genres"]
+                b["read_dates"] = ob.get("read_dates", b.get("read_dates", ""))
+                b["genres"] = ob.get("genres", b.get("genres", ""))
+                b["n_ratings"] = ob.get("n_ratings", b.get("n_ratings", ""))
 
     books_to_process = [
         b
@@ -176,35 +214,7 @@ def enhance_export(options: dict, captcha_solver: CaptchaSolver | None = None) -
         print(
             f"Book {i+1} of {len(books_to_process)}: {book['Title']} ({book['Author']})"
         )
-        book_id = book["Book Id"]
-
-        review_page = get_with_retry(session, make_review_url(book_id))
-        review_soup = BeautifulSoup(review_page.content, "html.parser")
-        read_dates = get_read_dates(review_soup)
-        book["read_dates"] = ";".join(
-            ",".join(d.strftime("%Y-%m-%d") if d else "" for d in reading)
-            for reading in read_dates
-        )
-
-        book_page = get_with_retry(session, make_book_url(book_id)).content.decode(
-            "utf-8"
-        )
-        n_ratings_match = re.search(r'"ratingsCount"\s*:\s*(\d+)', book_page)
-        if n_ratings_match is None:
-            raise ValueError("Did not find number of ratings in book page!")
-        book["n_ratings"] = n_ratings_match.group(1)
-
-        shelves_url_match = re.search('"[^"]*(work/shelves[^"]+)"', book_page)
-        if shelves_url_match is None:
-            raise ValueError("Did not find link to shelves page on book page!")
-        shelves_url = AbsoluteUrl(f"{BASE_URL}/{shelves_url_match.group(1)}")
-
-        genres_page = get_with_retry(session, shelves_url)
-        genres_soup = BeautifulSoup(genres_page.content, "html.parser")
-        genres = get_genres(genres_soup)
-        book["genres"] = ";".join(
-            f"{','.join(genre[0])}|{genre[1]}" for genre in genres
-        )
+        update_book_data(book, session)
 
         if i % 20 == 19 or i == len(books_to_process) - 1:
             print("saving csv")
